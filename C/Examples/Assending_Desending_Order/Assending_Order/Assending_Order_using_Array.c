2faff0d6e3c454eda63aa786378ad66469b6d7bc
Arun <adalurarun@users.noreply.github.com>
2017-03-28 13:59:14 +0530

    Create Assending_Order_using_Array.c

---
+++ C/Examples/Assending_Desending_Order/Assending_Order/Assending_Order_using_Array.c
@@ -0,0 +1,51 @@
+//Write a program to arrange N given numbers in assending order
+#include<stdio.h>
+int main()
+{
+  int i, j, n, a[50], temp;
+  printf("Give the value of N :\n"); // maximum N value is 50.
+  scanf("%d",&n);
+  printf("Enter the numbers one by one\n");
+  for(i=0;i<n;i++)  scanf("%d",&a[i]);
+  for(i=0;i<n;i++)
+  {
+  	for(j=i+1;j<n;j++)
+  	{
+  		if(a[i]>a[j])
+  		{
+  			temp = a[i];
+  			a[i] = a[j];
+  			a[j] = temp;
+		  }
+	}
+  }
+  printf("Printing the list = %d \n");
+  for (i=0;i<n;i++) printf("%d\t",a[i]);
+
+  return 0;
+}
+
+/* Output
+
+Give the value of N :
+4
+Enter the numbers one by one
+33
+16
+999
+670
+Printing the list = 999
+16      33      670     999
+*/
+
+/* Output is
+Give the value of N :
+5
+Enter the numbers one by one
+10
+30
+15
+24
+1
+small number = 1
+*/

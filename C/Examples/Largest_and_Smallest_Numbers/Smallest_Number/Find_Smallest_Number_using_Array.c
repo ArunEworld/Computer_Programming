fcde08d8360a6cb64512d7e1e22a96a3d8f0e6ed
Arun <adalurarun@users.noreply.github.com>
2017-03-28 13:48:00 +0530

    Create Find_Smallest_Number_using_Array.c

---
+++ C/Examples/Largest_and_Smallest_Numbers/Smallest_Number/Find_Smallest_Number_using_Array.c
@@ -0,0 +1,30 @@
+//Write a program to find the smallest of N numbers using array
+#include<stdio.h>
+int main()
+{
+  int i, n, a[50], small;
+  printf("Give the value of N :\n"); // maximum N value is 50.
+  scanf("%d",&n);
+  printf("Enter the numbers one by one\n");
+  for(i=0;i<n;i++)  scanf("%d",&a[i]);
+  small = a[0];
+  for(i=1;i<n;i++)
+  {
+    if(a[i]<small)  small = a[i];
+  }
+  printf("small number = %d", small);
+
+  return 0;
+}
+
+/* Output is
+Give the value of N :
+5
+Enter the numbers one by one
+10
+30
+15
+24
+1
+small number = 1
+*/

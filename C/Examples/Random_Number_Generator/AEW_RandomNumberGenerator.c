a677d6f596234ac40d41ea6f663877e6017e9334
Arun <adalurarun@users.noreply.github.com>
2017-06-12 11:11:32 +0530

    Create AEW_RandomNumberGenerator.c

--- C/Examples/Random_Number_Generator/AEW_RandomNumberGenerator.c
+++ C/Examples/Random_Number_Generator/AEW_RandomNumberGenerator.c
@@ -1,3 +1,4 @@
+/////************************ two Digit ***************//
 //Refer : https://www.tutorialspoint.com/c_standard_library/c_function_rand.htm
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,5 +31,38 @@ int main()
 
 
 
+
+*/
+
+/**************** five Digit ****************************************/
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main() {
+  int c, n;
+ 
+  printf("Ten random numbers in [1,100]\n");
+ 
+  for (c = 1; c <= 10; c++) {
+    n = rand() % 100000+ 1;
+    printf("%d\n", n);
+  }
+ 
+  return 0;
+}
+/* Output *
+42
+18468
+6335
+26501
+19170
+15725
+11479
+29359
+26963
+24465
 
 */

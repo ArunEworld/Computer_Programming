bd8712adbe5473221b3fff3cd89ed3812a3952dd
Arun <arun9047474939@gmail.com>
2017-12-30 01:40:06 +0530

    Update Decimal_to_Binary.c

--- C/Examples/Decimal_to_Binary.c
+++ C/Examples/Decimal_to_Binary.c
@@ -19,3 +19,38 @@ int main()
 	}
 	return 0;
 }
+
+/**************************************************************************/
+
+/*
+ * C Program to Print Binary Equivalent of an Integer using Recursion
+ */
+#include <stdio.h>
+ 
+int binary_conversion(int);
+
+int main()
+{
+	int num, bin;
+	printf("Enter a decimal number: ");
+	scanf("%d", &num);
+	bin = binary_conversion(num);
+	printf("The binary equivalent of %d is %d\n", num, bin);
+}
+
+int binary_conversion(int num)
+{
+	if (num == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		return (num % 2) + 10 * binary_conversion(num / 2);
+	}
+}
+
+/* Output
+	Enter a decimal number:  10
+	The binary equivalent of 10 is 1010
+*/

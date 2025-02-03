2fa57c2c1b46a5f2076e77fc88ebe8251661bdec
Arun <arun9047474939@gmail.com>
2025-02-03 19:15:46 +0530

    Added Example-2
    Added Example-2

--- C/Examples/Ascending_Descending_Order/Ascending_Order_using_Array.c
+++ C/Examples/Ascending_Descending_Order/Ascending_Order_using_Array.c
@@ -1,3 +1,4 @@
+//Example-1:
 //Write a program to arrange N given numbers in ascending
 #include<stdio.h>
 int main()
@@ -48,4 +49,49 @@ Enter the numbers one by one
 24
 1
 small number = 1
+*/
+
+/*****************************************************************************/
+//Example-2:
+#include <stdio.h>
+
+int main()
+{
+    int aew_temp, aew_arr[5]={20,30,10,25,1};
+    
+    printf("ArunEworld default array values [%d]: ",(int)(sizeof(aew_arr)/sizeof(int)));
+    
+    for(int i=0;i<5;i++)
+    {
+        printf("%d ",i[aew_arr]);
+    }
+        
+    printf("\n\r");
+    
+    for(int j=0;j<(sizeof(aew_arr)/sizeof(int))-1;j++)
+    {
+        for(int i=0;i<(sizeof(aew_arr)/sizeof(int))-1;i++)
+        {
+            if(aew_arr[i]>aew_arr[i+1])
+            {
+                aew_temp = aew_arr[i];
+                aew_arr[i] = aew_arr[i+1];
+                aew_arr[i+1] = aew_temp;
+            }
+        }
+    }
+    
+    printf("ArunEworld array Assending order values [%d]: ",(int)(sizeof(aew_arr)/sizeof(int)));
+    for(int i=0;i<5;i++)
+        printf("%d ",i[aew_arr]);
+
+    return 0;
+}
+
+/* Output
+ArunEworld default array values [5]: 20 30 10 25 1 
+ArunEworld array Assending order values [5]: 1 10 20 25 30 
+
+...Program finished with exit code 0
+Press ENTER to exit console.
 */
